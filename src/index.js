78f18cffa6c8a007b0fdf426b9d72d444151ab0f
cleyjoao <arruda.cleyton@gmail.com>
2025-06-20 12:35:43 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,4 @@
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import './index.css'; // ← esta se repite
